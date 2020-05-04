b73a4f1ac338847a867d5cd7eac581c93942f3ad
Corey Gwin <coreygwin@gmail.com>
2020-05-04 08:14:35 -0700

    WIP — Add OT for each Slate Op

--- src/plugin/with-collab.tsx
+++ src/plugin/with-collab.tsx
@@ -1,7 +1,7 @@
 import * as ShareDB from 'sharedb/lib/client'
 
 import { CollabEditor } from './collab-editor'
-import { Editor } from 'slate'
+import { Editor, Node, Path, Text } from 'slate'
 import { ReactEditor } from 'slate-react'
 import json1 from 'ot-json1'
 
@@ -42,30 +42,142 @@ export const withCollab = <T extends Editor & ReactEditor>(editor: T, config: Co
 
     const draftEditor = { ...e }
 
-    // TODO: Selection is not handled correctly
-    switch (slateOp.type) {
-      case 'insert_text':
-        const { path, offset, text } = slateOp
-        const inserTextJson1Op = [
-          json1.editOp(CollabEditor.pathToJson1Path(path).concat(['text']), 'text-unicode', [offset, text]),
-        ]
-        return e.doc.data && sendOp(inserTextJson1Op)
-      case 'insert_node':
-      case 'move_node':
-      case 'remove_node':
-      case 'remove_text':
-      case 'set_node':
-      case 'set_selection':
-      case 'merge_node':
-      case 'split_node':
-        console.log('before transform:', draftEditor.children?.[0]?.children, draftEditor.selection)
-        Editor.transform(draftEditor, slateOp)
-        console.log('after transform:', draftEditor.children?.[0]?.children, draftEditor.selection)
-        const json1Op = [
-          json1.replaceOp(['value'], editor.children, draftEditor.children),
-          json1.replaceOp(['selection'], editor.selection, draftEditor.selection),
-        ]
-        return e.doc.data && sendOp(json1Op)
+    apply(slateOp)
+
+    if (e.doc.data) {
+      switch (slateOp.type) {
+        case 'insert_node': {
+          const { path, node } = slateOp
+          const insertNodeJson1Op = [json1.insertOp(CollabEditor.pathToJson1Path(path), node)]
+          sendOp(insertNodeJson1Op)
+          break
+        }
+        case 'insert_text': {
+          const { path, offset, text } = slateOp
+          const inserTextJson1Op = [
+            json1.editOp(CollabEditor.pathToJson1Path(path).concat(['text']), 'text-unicode', [offset, text]),
+          ]
+          sendOp(inserTextJson1Op)
+          break
+        }
+        case 'merge_node': {
+          const { path } = slateOp
+          const node = Node.get(editor, path)
+          const prevPath = Path.previous(path)
+          const prev = Node.get(editor, prevPath)
+          let mergeNodeJson1Op: json1.JSONOp[] = []
+          if (Text.isText(node) && Text.isText(prev)) {
+            mergeNodeJson1Op.push(
+              json1.editOp(CollabEditor.pathToJson1Path(prevPath).concat(['text']), 'text-unicode', [
+                prev.text.length,
+                node.text,
+              ])
+            )
+          } else if (!Text.isText(node) && !Text.isText(prev)) {
+            mergeNodeJson1Op.push(
+              json1.insertOp(
+                CollabEditor.pathToJson1Path(prevPath).concat(['children', prev.children.length]),
+                node.children
+              )
+            )
+          }
+          mergeNodeJson1Op.push(json1.removeOp(CollabEditor.pathToJson1Path(path)))
+          console.log(slateOp.type, mergeNodeJson1Op)
+          sendOp(mergeNodeJson1Op)
+          break
+        }
+        case 'move_node': {
+          const { path, newPath } = slateOp
+          const moveNodeJson1Op = [
+            json1.moveOp(CollabEditor.pathToJson1Path(path), CollabEditor.pathToJson1Path(newPath)),
+          ]
+          console.log('move_node', moveNodeJson1Op)
+          sendOp(moveNodeJson1Op)
+          break
+        }
+        case 'remove_node': {
+          const { path } = slateOp
+          const removeNodeJson1Op = [json1.removeOp(CollabEditor.pathToJson1Path(path))]
+          console.log('remove_node', removeNodeJson1Op)
+          sendOp(removeNodeJson1Op)
+          break
+        }
+        case 'remove_text': {
+          const { path, offset, text } = slateOp
+          const removeTextJson1Op = [
+            json1.editOp(CollabEditor.pathToJson1Path(path).concat(['text']), 'text-unicode', [
+              offset,
+              { d: text.length },
+            ]),
+          ]
+          console.log('remove_text', removeTextJson1Op)
+          sendOp(removeTextJson1Op)
+          break
+        }
+        case 'set_node': {
+          const { path, newProperties } = slateOp
+          const json1Path = CollabEditor.pathToJson1Path(path)
+          const setNodeJson1Op = Object.keys(newProperties).map(key => {
+            const value = newProperties[key]
+            return value === null
+              ? json1.removeOp(json1Path.concat([key]))
+              : json1.insertOp(json1Path.concat([key]), value)
+          })
+          console.log('set_node', setNodeJson1Op)
+          sendOp(setNodeJson1Op)
+          break
+        }
+        case 'set_selection': {
+          const { newProperties } = slateOp
+          const setSelectionJson1Op = [json1.replaceOp(['selection'], editor.selection, newProperties)]
+          console.log('set_selection', setSelectionJson1Op)
+          return e.doc.data && sendOp(setSelectionJson1Op)
+        }
+        case 'split_node':
+          const { path, position, properties } = slateOp
+
+          const node = Node.get(editor, path)
+
+          let newPath = path.map((p, i) => (i === path.length - 1 ? p + 1 : p))
+
+          let splitNodeJson1Op: json1.JSONOp[] = []
+          if (Text.isText(node)) {
+            const textBefore = node.text.slice(0, position)
+            const textAfter = node.text.slice(position)
+            const newNode = {
+              ...node,
+              text: textBefore,
+            }
+            const splitNode = {
+              ...newNode,
+              ...(properties as Partial<Text>),
+              text: textAfter,
+            }
+            console.log('splitNode - text', { textBefore, textAfter, splitNode })
+
+            splitNodeJson1Op.push(json1.insertOp(CollabEditor.pathToJson1Path(newPath), splitNode))
+          } else {
+            const nodesBefore = node.children.slice(0, position)
+            const nodesAfter = node.children.slice(position)
+            const newNode = {
+              ...node,
+              children: nodesBefore,
+            }
+            const splitNode = {
+              ...newNode,
+              ...(properties as Partial<Element>),
+              children: nodesAfter.length === 0 ? [{ text: '' }] : nodesAfter,
+            }
+
+            console.log('splitNode - element', { nodesBefore, nodesAfter, splitNode })
+
+            splitNodeJson1Op.push(json1.insertOp(CollabEditor.pathToJson1Path(newPath), splitNode))
+          }
+
+          console.log(slateOp.type, splitNodeJson1Op)
+          sendOp(splitNodeJson1Op)
+          break
+      }
     }
   }
 
